8af3550a2731e0615e8f41f648399d8814e4a0f1
Ramon <ramonkolb@gmail.com>
2020-05-18 12:39:33 -0400

    minor change

--- scripts/catchup.sh
+++ scripts/catchup.sh
@@ -3,12 +3,13 @@ OUTFILEDIR=/usr/share/dump1090-fa/html/planefence
 OUTFILEBASE=$OUTFILEDIR/planefence
 OUTFILETODAY=$OUTFILEBASE-`date --date="today" '+%y%m%d'`.html
 HTMLFOOTER=".html"
+PLANEFENCEDIR=/usr/share/planefence
 
 for t in /tmp/dump1090-127*.txt;
 do [[ -e $t ]] | continue;
    OUTFILE=$OUTFILEBASE-${t:24:6}
    echo processing ${t:24:6}
-   /home/pi/planefence/planefence.py --logfile=$t --outfile=$OUTFILE --format=both --maxalt=5000 --verbose
+   $PLANEFENCEDIR/planefence.py --logfile=$t --outfile=$OUTFILE --format=both --maxalt=5000 --verbose
    # ln -sf $OUTFILETODAY $OUTFILEDIR/index.html
    printf "<p><p style=\"font-size:10px\">History:\n" >> $OUTFILE.html
    printf "<p style=\"font-size:10px\"><a href=\"/planefence\">Latest</a> | " >> $OUTFILE.html
