3d74b27a93b947e2cde72ed8fcef270d37142f8d
Ramon <ramonkolb@gmail.com>
2020-06-18 16:12:29 -0400

    Update install_planefence.sh

--- install_planefence.sh
+++ install_planefence.sh
@@ -29,7 +29,7 @@
 # You should have received a copy of the GNU General Public License along with this program.
 # If not, see https://www.gnu.org/licenses/.
 clear
-echo "Welcome to PlaneFence Setup - version200618-1600"
+echo "Welcome to PlaneFence Setup - version200618-1615"
 echo "https://github.com/kx1t/planefence"
 echo "Copyright 2020 by Ramón F. Kolb, with collaborations by Rodney Yeo"
 echo ""
