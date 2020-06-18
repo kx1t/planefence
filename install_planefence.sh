39bed0d25dbdeae3dd1444c8e3bfb9fb2d2aa538
Ramon <ramonkolb@gmail.com>
2020-06-18 16:25:09 -0400

    Update install_planefence.sh

--- install_planefence.sh
+++ install_planefence.sh
@@ -292,7 +292,7 @@ echo ""
 echo "--------------------------------------------------------------------"
 echo ""
 echo "Done!"
-echo "You can find more settings in /usr/share/planefence/planefence.conf
+echo "You can find more settings in /usr/share/planefence/planefence.conf"
 echo "Feel free to look at those and change them to your liking."
 echo ""
 echo "Sayonara! Best regards 
